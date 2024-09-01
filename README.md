369a8203f2addf9f4270536cb7e3c97b0b554834
adisuyash <adityasuyashgupta@gmail.com>
2024-09-02 02:18:38 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 [//]: <> (README.md file for Tech Bastic website)
 # Tech Bastic
+An open source community helping you to become a self-taught developer!
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Tech Bastic</h1>
+      <p className="open-source">
+        An open source community helping you to become a self-taught developer!
+      </p>
+      <a href="https://discord.com/invite/MSRnjkHcxK" target="_blank" rel="noopener noreferrer">
+        <button>Join Now!</button>
+      </a>
+    </>
+  );
+}
+
+export default App;
